8dad78c509b041ae8e0d8d81d37c82671f218b7c
ved_patel <ved@auberginesolutions.com>
2024-12-30 15:31:08 +0530

    add homepage

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('users.urls')),
     path('company/', include('company.urls')),
     path('job/', include('job.urls')),
