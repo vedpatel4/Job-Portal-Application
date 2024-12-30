4636f5a46651487df41303d209327b47a6600aa1
ved_patel <ved@auberginesolutions.com>
2024-12-30 14:32:10 +0530

    add job_update functionality

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create,show
+from .views import create,show,update
 
 urlpatterns = [
     path('create/', create, name='create'),
     path('show/', show, name='show'),
+    path('update/<int:id>/', update, name='update'),
 ]
--- job/views.py
+++ job/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Job
 from .forms import Create_JobForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 @login_required
 def create(request):
@@ -22,5 +23,25 @@ def show(request):
     jobs = Job.objects.all()
     return render(request, "job/show.html", {'jobs': jobs})
 
+@login_required
+def update(request, id):
+    job = get_object_or_404(Job, id=id)
+    if job.user != request.user:
+        return HttpResponseForbidden("You are not allowed to edit this job.")
+    
+    form = Create_JobForm(request.POST or None, instance=job)
+    if form.is_valid():
+        form.save()
+        return redirect("/")
+    
+    context = {'updatejobform': form}
+    return render(request, 'job/update.html', context=context)
 
-
+# @login_required
+# def destroy_job(request, id):
+#     job = get_object_or_404(Job, id=id)
+#     if job.user != request.user:
+#         return HttpResponseForbidden("You are not allowed to delete this job.")
+    
+#     job.delete()
+#     return redirect("/")
